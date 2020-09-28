4278ef0025bf64c3109c2fbca91f31162f0bdbdb
masayoshi.suzuki <sp1103masa@gmail.com>
2020-09-28 15:09:13 +0900

    Initial Commit

---
+++ SliedeshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliedeshowApp
+//
+//  Created by 鈴木正義 on 2020/09/28.
+//  Copyright © 2020 masayoshi.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
